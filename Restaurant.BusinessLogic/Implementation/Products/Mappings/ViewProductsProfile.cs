8f7745cbb70f27fa374f4714d1689dea03e3d534
iuliastaci <iuliastaci@users.noreply.github.com>
2024-05-14 12:42:59 +0300

    Add Product/ViewProducts

---
+++ Restaurant.BusinessLogic/Implementation/Products/Mappings/ViewProductsProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Restaurant.BusinessLogic.Implementation.Products.Models;
+using Restaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.BusinessLogic.Implementation.Products.Mappings
+{
+    public class ViewProductsProfile : Profile
+    {
+        public ViewProductsProfile()
+        {
+            CreateMap<Product, ViewProductModel>();
+                /*.ForMember(a => a.Picture, a => a.MapFrom(s => s.Picture))
+                .ForMember(a => a.Subcategory, a => a.MapFrom(s => s.Subcategory));*/
+        }
+    }
+}
---
+++ Restaurant.BusinessLogic/Implementation/Products/Models/ViewProductModel.cs
@@ -0,0 +1,19 @@
+﻿using Restaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.BusinessLogic.Implementation.Products.Models
+{
+    public class ViewProductModel
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public Subcategory Subcategory { get; set; }
+    }
+}
