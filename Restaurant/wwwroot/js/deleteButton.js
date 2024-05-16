ff112ca80763d0b74f1c35857c53826966af024d
iuliastaci <iuliastaci@users.noreply.github.com>
2024-05-16 21:51:49 +0300

    Created Javascript file for Delete button in Product/Index

---
+++ Restaurant/wwwroot/js/deleteButton.js
@@ -0,0 +1,23 @@
+﻿$(".deleteButton").click(function (event) {
+    event.preventDefault();
+
+    let editButton = $(this).closest('.buttonsDiv').find('.editButton');
+
+    editButton.hide();
+
+    let noButton = $(this).closest('.buttonsDiv').find('.noButton');
+
+    let yesButton = $(this).closest('.buttonsDiv').find('.yesButton');
+
+    noButton[0].onclick = () => {
+        yesButton.hide();
+        noButton.hide();
+        editButton.show();
+        $(this).show();
+    }
+
+    $(this).hide();
+
+    noButton.show();
+    yesButton.show();
+});
--- Restaurant/wwwroot/js/site.js
+++ Restaurant/wwwroot/js/site.js
@@ -3,26 +3,3 @@
 
 // Write your JavaScript code.
 
-$(".deleteButton").click(function (event) {
-    event.preventDefault();
-
-    let editButton = $(this).closest('.buttonsDiv').find('.editButton');
-
-    editButton.hide();
-
-    let noButton = $(this).closest('.buttonsDiv').find('.noButton');
-
-    let yesButton = $(this).closest('.buttonsDiv').find('.yesButton');
-
-    noButton[0].onclick = () => {
-        yesButton.hide();
-        noButton.hide();
-        editButton.show();
-        $(this).show();
-    }
-
-    $(this).hide();
-
-    noButton.show();
-    yesButton.show();
-});
